8af0e8af4465efe6e32990366992adf9a0baa8ec
hyukjin0419 <21900771@handong.ac.kr>
2025-12-11 11:58:54 +0900

    first commit

---
+++ src/main/java/com/pard/server/brewnotebackend/BrewNoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pard.server.brewnotebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewNoteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewNoteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pard/server/brewnotebackend/BrewNoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pard.server.brewnotebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewNoteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
